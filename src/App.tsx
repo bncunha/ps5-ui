226ee182943902e5b754f42fc77df693eac2fb9d
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-12-07 12:00:16 -0300

    add noreferer

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SelectPlayer from './pages/SelectPlayer/SelectPlayer';
 function App() {
   return (
     <>
-      <a className="github-link" href="https://github.com/bncunha/ps5-ui" target="_blank">
+      <a className="github-link" href="https://github.com/bncunha/ps5-ui" target="_blank" rel="noreferrer">
         <img src="https://pngimg.com/uploads/github/github_PNG83.png" alt="GitHub"/>
       </a>
       <SelectPlayer></SelectPlayer>
